aac9715b2466afffec436cbc111024fe03e86cf0
Max Zhao <max.zhao@arsbrevis.de>
2023-01-30 10:26:43 +0100

    Make cron work

--- README.md
+++ README.md
@@ -14,3 +14,9 @@ Main systems include:
 `./mdb_to_mail.py`
   Extracts appointments for individual users and sends them a mail with
   appointments for the next 14 days.
+
+## Baserow import
+
+`./fetch_baserow_schemas.py`
+  Extract baserow schemas into yaml format. This is used to manually add the
+  mapping from our tnamse export.
--- cron.sh
+++ cron.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+MAMBA_EXE="/home/zhaom/.local/bin/micromamba";
+MAMBA_ROOT_PREFIX="/home/zhaom/micromamba";
+PATH="/home/zhaom/micromamba/condabin:/home/zhaom/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/home/zhaom/.zgenom/sources/unixorn/fzf-zsh-plugin/___/bin:/home/zhaom/.fzf/bin:/home/zhaom/.zgenom/sources/unixorn/fzf-zsh-plugin/___/bin"
+
 cd "$(dirname "$0")"
 . /home/zhaom/micromamba/etc/profile.d/micromamba.sh
 micromamba activate cads_data_exchange
