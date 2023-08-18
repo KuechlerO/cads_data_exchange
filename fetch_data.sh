c1e2cc24782581dbb918482334cf7d85a405d61d
Max Zhao <max.zhao@arsbrevis.de>
2023-08-19 00:00:00 +0200

    Fixing bugs

--- fetch_data.sh
+++ fetch_data.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-source ./.env
 SODAR_PROJECT=f2acceb7-067d-41a4-8e39-236c022678f1
 
 sodar_dir="data/sodar"
@@ -8,14 +7,15 @@ varfish_file="$varfish_dir/cases_$(date +"%Y-%m-%d").json"
 pel_file="data/lb_pel.tsv"
 clinicians_file="data/clinicians.json"
 case_file="data/cases.json"
+pel_json_file="data/lb_pel.json"
 
 mkdir -p "$sodar_dir"
 mkdir -p "$varfish_dir"
 
 NO_PROXY="bihealth.org" sodar-cli samplesheet export --overwrite --write-output $sodar_dir $SODAR_PROJECT
 NO_PROXY="bihealth.org" varfish-cli case --output-format json --output-file $varfish_file list $SODAR_PROJECT
-NO_PROXY="laborberlin.intern" python ./fetch_lb_pel.py --username $PEL_USER --password $PEL_PASSWORD "$pel_file"
 NO_PROXY="charite.de" python ./fetch_baserow_table.py "$clinicians_file"
 NO_PROXY="charite.de" python ./fetch_baserow_table.py --table_id 579 "$case_file"
+NO_PROXY="charite.de" python ./fetch_baserow_table.py --table_id 660 "$pel_json_file"
  
 ./cleanup_old.py $varfish_dir
--- generate_batch_email.py
+++ generate_batch_email.py
@@ -1,5 +1,6 @@
 import re
 import json
+from dataclasses import dataclass
 import pandas as pd
 
 import datetime
@@ -83,210 +84,29 @@ def matchLbId(lbid1, lbid2):
     return (m1.group(1), m1.group(2)) == (m2.group(1), m2.group(2))
 
 
-def matchName(name1, name2, strict=True):
-    """Match names for two persons. Ignores case and special characters.
-
-    If strict is set names will be directly compared.
-    """
-
-    n1 = unidecode(name1).lower().replace("-", " ")
-    n2 = unidecode(name2).lower().replace("-", " ")
-
-    # provide direct comparison if strict
-    if strict:
-        return n1 == n2
-
-    # compare first name if not strict
-    nparts1 = n1.split(" ")
-    nparts2 = n2.split(" ")
-    return nparts1[0] == nparts2[0]
-
-def format_date(datestr):
-    if datestr is None:
-        return datestr
-    if "." in datestr:
-        datestr = datetime.datetime.strptime(datestr, "%d.%m.%Y").date().isoformat()
-    return datestr
-
-def matchDate(date1, date2):
-    """Match 2 dates.
-    """
-    d1 = format_date(date1)
-    d2 = format_date(date2)
-    return d1 == d2
-
-
-def matchSample(s, index_entry):
-
-    return (
-        matchLbId(s["LB ID"], index_entry["LB-ID"])
-    ) or (
-        matchName(s["Lastname"], index_entry["Fam Name"], strict=True)
-        and
-        matchName(s["Firstname"], index_entry["Given Name"], strict=False)
-        and
-        matchDate(s["Birthdate"], index_entry["Birthdate"])
-    )
-
-
-def match_last_name(name, other):
-    if name == other:
-        return True
-
-    name_parts = name.split("-")
-    other_parts = other.split("-")
-    if name_parts[0] == other_parts[0]:
-        return True
-    return False
-
-
-def matchPerson(persons, first=None, last=None, short=None):
-    for person in persons:
-        matches = []
-        if first:
-            matches.append(first == person["Firstname"])
-        if last:
-            matches.append(match_last_name(person["Lastname"], last))
-        if short:
-            matches.append(short == person["Shorthand"])
-        if matches and all(matches):
-            return person
-    return None
-
-
-def matchAny(persons, name_string):
-    names = [n.strip() for n in name_string.split("/")]
-    for name in names:
-        if p := matchPerson(persons, last=name):
-            return p
-    return None
-
-
-def infer_analysis_type(num_entries, analysis_type):
-    if num_entries == 1:
-        return "Single-Genome"
-    types = {
-        1: "Single",
-        2: "Duo",
-        3: "Trio",
-        4: "Quattro",
-    }
-    count_name = types.get(num_entries, num_entries)
-    return f"{count_name}-{analysis_type}"
-
-
-def gather_info(fam_id, cases_data, lb_data, varfish_data):
-    varfish_fam = varfish_data.loc[varfish_data["name"].apply(lambda n: matchLbId(fam_id, n))].to_dict("records")
-
-    lb_fam = lb_data.loc[lb_data["Index-ID"].apply(lambda n: matchLbId(fam_id, n))].to_dict("records")
-
-    lb_index = [l for l in lb_fam if l["Index-ID"] == l["LB-ID"]]
-    if len(lb_index) == 0:
-        lb_index = [l for l in lb_fam if matchLbId(l["Index-ID"], l["LB-ID"])]
-
-    if len(lb_index) != 1:
-        print(f"{fam_id} index samples not exactly 1: {lb_index}")
-        return None
-    if len(lb_index) >= 1:
-        lb_index = lb_index[0]
-    else:
-        lb_index = {
-            "Fam Name": "",
-            "Given Name": "",
-            "Birthdate": "",
-            "LB-ID": fam_id,
-        }
-
-    # found in baserow
-    cases_index_found = [(eid, edata) for eid, edata in cases_data.items() if matchSample(edata, lb_index)]
-
-    lbid_fmt = fam_id.replace("_", "-")
-    firstname_fmt = ""
-    lastname_fmt = ""
-    birthdate_fmt = ""
-    sender_fmt = ""
-    firstlook_fmt = ""
-    errors = []
-    entry_id = None
-    tnamse_index = None
-    if lb_index:
-        firstname_fmt = lb_index["Given Name"]
-        lastname_fmt = lb_index["Fam Name"]
-        birthdate_fmt = format_date(lb_index["Birthdate"])
-        lbid_fmt = lb_index["LB-ID"]
-    if cases_index_found:
-        if len(cases_index_found) == 1:
-            (entry_id, tnamse_index), = cases_index_found
-        elif len(cases_index_found) > 1:
-            index_candidates = [(i, d) for i, d in cases_index_found if d["LB ID"] == lb_index["LB-ID"]]
-            if len(index_candidates) == 1:
-                (entry_id, tnamse_index), = index_candidates
-            else:
-                print("Duplicate baserow entries {[i for i, _ in index_candidates]} with same LB ID: {lbid_fmt}")
-        else:
-            found_data = [(i, d["LB ID"]) for i, d in cases_index_found]
-            print(f"Multiple index cases found in baserow: {found_data}")
-
-        if lb_index:
-            if lb_index["Given Name"] != tnamse_index["Firstname"]:
-                firstname_fmt = f"TN({tnamse_index['Firstname']})|LB({lb_index['Given Name']})"
-                errors.append(("firstname", firstname_fmt))
-            if lb_index["Fam Name"] != tnamse_index["Lastname"]:
-                lastname_fmt = f"TN({tnamse_index['Lastname']})|LB({lb_index['Fam Name']})"
-                errors.append(("lastname", lastname_fmt))
-            if (d1 := format_date(lb_index["Birthdate"])) != (d2 := format_date(tnamse_index["Birthdate"])):
-                birthdate_fmt = f"TN({d1})|LB({d2})"
-                errors.append(("birthdate", birthdate_fmt))
-            if tnamse_index["LB ID"] not in (None, "") and not matchLbId(lb_index["LB-ID"], tnamse_index["LB ID"]):
-                lbid_fmt = f"TN({tnamse_index['LB ID']})|LB({lb_index['LB-ID']})"
-                errors.append(("lbid", lbid_fmt))
-        sender_fmt = tnamse_index["Clinician"]
-        firstlook_fmt = tnamse_index["First Look"]
-    else:
-        print(lb_index, " not found")
-
+def analysezahl_to_int(analysezahl):
     return {
-        "fam_id": fam_id,
-        "lb_id": lbid_fmt,
-        "firstname": firstname_fmt,
-        "lastname": lastname_fmt,
-        "birthdate": birthdate_fmt,
-        "sender": sender_fmt,
-        "firstlook": firstlook_fmt,
-        "errors": errors,
-        "varfish_found": bool(varfish_fam),
-        "lb_found": bool(lb_index),
-        "varfish_data": varfish_fam[0] if varfish_fam else None,
-        "lb_data": lb_index,
-        "baserow_data": tnamse_index,
-        "baserow_id": entry_id,
-    }
+            "Single": 1,
+            "Duo": 2,
+            "Trio": 3,
+            "Quattro": 4,
+    }.get(analysezahl, 0)
 
 
 def format_info_line(info, clinicians_data):
-    if not info["firstlook"]:
+    if not info["First Look"]:
         firstlook_fmt = "MISSING"
     else:
-        firstlook_datas = [clinicians_data[str(i)] for i in info["firstlook"]]
+        firstlook_datas = [clinicians_data[str(i)] for i in info["First Look"]]
         firstlook_fmt = ",".join(fl["Lastname"] for fl in firstlook_datas)
 
-    if not info["sender"]:
+    if not info["Clinician"]:
         sender_fmt = "MISSING"
     else:
-        sender_datas = [clinicians_data[str(i)] for i in info["sender"]]
+        sender_datas = [clinicians_data[str(i)] for i in info["Clinician"]]
         sender_fmt = ",".join(d["Lastname"] for d in sender_datas)
 
-    notifications = []
-    if not info["varfish_found"]:
-        notifications.append("not in varfish")
-    if not info["lb_found"]:
-        notifications.append("not in LB PEL")
-    if info["baserow_id"] is None:
-        notifications.append("not in Baserow")
-    notification = ",".join(notifications)
-
-    error = ",".join(f"{n}({err})" for n, err in info["errors"])
-    info_line = f"{info['fam_id']}({info['lastname']} {info['birthdate']}) Einsender: {sender_fmt} First Look: {firstlook_fmt} {notification} {error}"
+    info_line = f"{info['LB ID']}({info['Lastname']} {info['Birthdate']}) Einsender: {sender_fmt} First Look: {firstlook_fmt}"
     return info_line
 
 
@@ -317,38 +137,6 @@ def status_newer(new_status, old_status):
     return new_index > old_index
 
 
-def update_baserow(data):
-    varfish_data = data["varfish_data"]
-    lb_data = data["lb_data"]
-
-    baserow_data = data["baserow_data"]
-    changed_fields = []
-    if lb_data["ProbenDate"] is not None and (m := re.search(r"\d{2}.\d{2}.\d{4}", lb_data["ProbenDate"])):
-        new_proben_date = datetime.datetime.strptime(m.group(0), "%d.%m.%Y").date().isoformat()
-        if baserow_data["Datum Labor"] != new_proben_date:
-            baserow_data["Datum Labor"] = new_proben_date
-            changed_fields.append("Datum Labor")
-
-    if baserow_data["Varfish"] in (None, "") and varfish_data is not None:
-        baserow_data["Varfish"] = f"https://varfish.bihealth.org/variants/f2acceb7-067d-41a4-8e39-236c022678f1/case/{varfish_data['sodar_uuid']}"
-        changed_fields.append("Varfish")
-
-    if baserow_data["LB ID"] in (None, "") and data["lb_id"]:
-        baserow_data["LB ID"] = data["lb_id"]
-        changed_fields.append("LB ID")
-
-    if varfish_data is not None:
-        cur_status = baserow_data["Case Status"]
-        varfish_conv_status = VARFISH_STATUS_TO_BASEROW[varfish_data["status"]]
-        if status_newer(varfish_conv_status, cur_status):
-            baserow_data["Case Status"] = varfish_conv_status
-            changed_fields.append("Case Status")
-
-    if changed_fields:
-        print(f"Updating baserow entry {data['baserow_id']} {baserow_data['LB ID']} Fields: {','.join(changed_fields)}")
-        BR.add_data(CASE_TABLE_ID, baserow_data, row_id=data["baserow_id"])
-
-
 def format_message(output_per_batch, padding=""):
     message_lines = []
     for name, lines in output_per_batch.items():
@@ -375,37 +163,229 @@ def send_email(title, message_text, recipients):
 
 
 def get_varfish_new(varfish_data, cases_data):
-    case_uuids = [c["Varfish"].split("/")[-1] for c in cases_data.values() if c["Varfish"]]
+    uuids_case = {c["Varfish"]: c["LB ID"] for c in cases_data.values() if c["Varfish"]}
+    case_uuids = {c["LB ID"]: c["Varfish"] for c in cases_data.values() if c["Varfish"]}
     case_comments = [c["Kommentar"] for c in cases_data.values()]
-    missing_uuids = [f"{l} ({i})" for i, l in zip(varfish_data["sodar_uuid"], varfish_data["name"]) if i not in case_uuids and not any((i in c) for c in case_comments if c)]
+    missing_uuids = []
+    for uuid, vname in zip(varfish_data["sodar_uuid"], varfish_data["name"]):
+        if uuid not in uuids_case and not any(matchLbId(vname, n) for n in case_uuids):
+            missing_uuids.append(f"{vname} ({uuid})")
     return missing_uuids
+
+
+@dataclass(frozen=True, eq=True)
+class NameInfo:
+    first: str
+    last: str
+    birthdate: datetime.date
+
+    @classmethod
+    def from_any(cls, first, last, birthdate):
+        if first is None:
+            first = ""
+        if last is None:
+            last = ""
+
+        first = unidecode(first).strip().replace("-", " ").lower().split()
+        last = unidecode(last).strip().replace("-", " ").lower().split()
+        if type(birthdate) is str:
+            if "-" in birthdate:
+                birthdate = datetime.datetime.strptime(birthdate, "%Y-%m-%d").date()
+            elif "." in birthdate:
+                birthdate = datetime.datetime.strptime(birthdate, "%d.%m.%Y").date()
+            else:
+                raise TypeError(f"Invalid date string: {birthdate}")
+        return cls(first=first, last=last, birthdate=birthdate)
+
+    def match(self, other):
+        birthdate_valid = (self.birthdate == other.birthdate) or not self.birthdate
+
+        name_valid = False
+        if not self.first or not self.last or not other.first or not other.last:
+            name_valid = False
+        elif self.first == other.first and self.last == other.last:
+            name_valid = True
+        elif self.first[0] == other.first[0] and self.last == other.last:
+            name_valid = True
+
+        if self.last == ["kratt"] and other.last == ['kratt']:
+            print(self, other, name_valid, birthdate_valid)
+
+        return birthdate_valid and name_valid
+
+
+def update_baserow_from_lb(cases_data, lb_data):
+    ID_FIELD = "LB ID"
+    INDEX_FIELD = "Index ID"
+    COUNT_FIELD = "Analysezahl"
+
+    def match_sample_by_sampleid(lb_data, sample_id):
+        found = []
+        for entry in lb_data.values():
+            if matchLbId(sample_id, entry.get(ID_FIELD)):
+                found.append(entry)
+        return found
+
+    def match_sample_by_indexid(lb_data, index_id):
+        found = []
+        for entry in lb_data.values():
+            if matchLbId(index_id, entry.get(INDEX_FIELD)):
+                found.append(entry)
+        return found
+
+    def match_sample_by_name(lb_data, firstname, lastname, birthdate):
+        name_info = NameInfo.from_any(firstname, lastname, birthdate)
+        found = []
+        for entry in lb_data.values():
+            entry_info = NameInfo.from_any(
+                entry["Firstname"], entry["Lastname"], entry["Birthdate"]
+            )
+            if name_info.match(entry_info):
+                found.append(entry)
+        return found
+
+    all_updates = {}
+    for bs_id, bs_entry in cases_data.items():
+        updates = {
+            "current": bs_entry,
+            "updates": []
+        }
+        bs_lbid = bs_entry[ID_FIELD]
+        bs_probendate = bs_entry["Datum Labor"]
+        bs_count = bs_entry[COUNT_FIELD]
+        bs_count_int = analysezahl_to_int(bs_count)
+        if bs_lbid:
+            lb_samples = match_sample_by_sampleid(lb_data, bs_lbid)
+        else:
+            lb_samples = match_sample_by_name(lb_data, bs_entry["Firstname"], bs_entry["Lastname"], bs_entry["Birthdate"])
+
+        if not lb_samples:
+            if not bs_lbid:
+                print(f"No LB PEL Entry matched for {bs_id} {bs_entry['Firstname']} {bs_entry['Lastname']} {bs_entry['Birthdate']} LBID({bs_lbid})")
+            continue
+        if len(lb_samples) > 1:
+            print(f"{len(lb_samples)} PEL entries matched for {bs_id} {bs_entry['Firstname']} {bs_entry['Lastname']} {bs_entry['Birthdate']} LBID({bs_lbid})")
+        lb_entry = lb_samples[-1]
+        lb_index = lb_entry[INDEX_FIELD]
+        lb_fam_entries = match_sample_by_indexid(lb_data, lb_index)
+
+        lb_lbid = lb_entry[ID_FIELD]
+        lb_probendate = lb_entry["Datum Labor"]
+
+        if not bs_lbid and lb_lbid:
+            updates["updates"].append((ID_FIELD, lb_lbid))
+        if not bs_probendate and lb_probendate:
+            if m := re.search(r"\d{2}.\d{2}.\d{4}", lb_entry["Datum Labor"]):
+                lb_probendate_fmt = datetime.datetime.strptime(m.group(0), "%d.%m.%Y").date().isoformat()
+                updates["updates"].append(("Datum Labor", lb_probendate_fmt))
+
+        # only autoupdate trio for now
+        if not bs_count and lb_fam_entries == 3:
+            updates["updates"].append(("Analysezahl", "Trio"))
+
+        if updates["updates"]:
+            all_updates[bs_id] = updates
+
+    return all_updates
+
+
+def match_cases(cases_data, fam_id, sample_ids):
+    found = {}
+    for bs_id, bs_entry in cases_data.items():
+        bs_lbid = bs_entry["LB ID"]
+        if matchLbId(bs_lbid, fam_id):
+            found[bs_id] = bs_entry
+        elif any(matchLbId(bs_lbid, sample_id) for sample_id in sample_ids):
+            found[bs_id] = bs_entry
+    return found
+
+
+def update_baserow_from_sodar(cases_data, sodar_data, varfish_data):
+
+
+    all_updates = {}
+
+    for fam_id, sodar_fam in sodar_data.groupby("Characteristics[Family]"):
+        varfish_fam = varfish_data.loc[varfish_data["name"].apply(lambda n: matchLbId(fam_id, n))].to_dict("records")
+        batch_ids = sodar_fam["Characteristics[Batch]"]
+        batch_id = int(batch_ids.values[0])
+        sample_ids = sodar_fam["Sample Name"]
+
+        if varfish_fam:
+            varfish_sodar_uuid = varfish_fam[0]["sodar_uuid"]
+            varfish_status = VARFISH_STATUS_TO_BASEROW[varfish_fam[0]["status"]]
+        else:
+            varfish_sodar_uuid = None
+            varfish_status = None
+
+        cases = match_cases(cases_data, fam_id, sample_ids)
+        if cases:
+            for bs_id, bs_entry in cases.items():
+                update = {"current": bs_entry, "updates": []}
+
+                if not bs_entry["Batch"] and batch_id:
+                    update["updates"].append(("Batch", batch_id))
+
+                if (not bs_entry["Varfish"] or "http" in bs_entry["Varfish"]) and varfish_sodar_uuid:
+                    update["updates"].append(("Varfish", varfish_sodar_uuid))
+
+                bs_status = bs_entry["Case Status"]
+                if varfish_status is not None and status_newer(varfish_status, bs_status):
+                    update["updates"].append(("Case Status", varfish_status))
+
+                if update["updates"]:
+                    if bs_id not in all_updates:
+                        all_updates[bs_id] = update
+                    else:
+                        all_updates[bs_id]["updates"] += update["updates"]
+
+    return all_updates
+
+
+def apply_updates_to_baserow(lb_updates):
+    for bs_id, bs_update in lb_updates.items():
+        entry = bs_update["current"].copy()
+        updates = bs_update["updates"]
+
+        print(f"Updating baserow entry {bs_id} {entry['Firstname']} {entry['Lastname']} *{entry['Birthdate']} Fields: {', '.join(f for f, _ in updates)}")
+        for field, value in updates:
+            entry[field] = value
+        BR.add_data(CASE_TABLE_ID, entry, row_id=bs_id)
 
 
 def main():
     paths = {
         "cases": "data/cases.json",
-        "lb": "data/lb_pel.tsv",
+        "lb": "data/lb_pel.json",
         "sodar": "data/sodar/s_CADS_Exomes_Diagnostics.txt",
         "varfish": "data/varfish",
         "clinicians": "data/clinicians.json",
     }
     cases_data = load_json(paths["cases"], cast=False)
+    lb_data = load_json(paths["lb"], cast=False)
     clinicians_data = load_json(paths["clinicians"], cast=False)
-    lb_data = load_tsv(paths["lb"])
     sodar_data = load_tsv(paths["sodar"])
     varfish_data = load_latest(paths["varfish"])
 
-    new_varfish_ids = get_varfish_new(varfish_data, cases_data)
+    lb_updates = update_baserow_from_lb(cases_data, lb_data)
+    if lb_updates:
+        print("There are baserow updates to be submitted.")
+        apply_updates_to_baserow(lb_updates)
 
+    sodar_updates = update_baserow_from_sodar(cases_data, sodar_data, varfish_data)
+    if sodar_updates:
+        print("There are baserow updates to be submitted.")
+        apply_updates_to_baserow(sodar_updates)
+
+    new_varfish_ids = get_varfish_new(varfish_data, cases_data)
     print("New varfish entries: ", ", ".join(new_varfish_ids))
 
     output_per_batch = {}
     for batch_id, sodar_batch in sodar_data.groupby("Characteristics[Batch]"):
         output_per_batch[batch_id] = []
         for fam_id, sodar_fam in sodar_batch.groupby("Characteristics[Family]"):
-            info = gather_info(fam_id, cases_data, lb_data, varfish_data)
-            if info["baserow_id"] is not None:
-                update_baserow(info)
+            matched_cases = match_cases(cases_data, fam_id, [])
+            info = list(matched_cases.values())[0]
             formatted = format_info_line(info, clinicians_data)
             output_per_batch[batch_id].append(formatted)
 
