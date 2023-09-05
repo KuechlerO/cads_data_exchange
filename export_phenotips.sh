a605f77d1b6be4106583801c1f7ede367cbd8ac7
Max Zhao <max.zhao@arsbrevis.de>
2023-09-05 19:50:03 +0200

    Adding phenotips export table

---
+++ export_phenotips.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+BACKUP_PATH="/media/BaserowExport"
+TMP_PATH="/tmp/bsexport"
+
+mkdir -p "$TMP_PATH"
+NO_PROXY="charite.de" python ./fetch_baserow_table.py --table_id 579 "${TMP_PATH}/Cases.xlsx"
+NO_PROXY="charite.de" python ./fetch_baserow_table.py --table_id 583 "${TMP_PATH}/AdditionalDiagnostics.xlsx"
+NO_PROXY="charite.de" python ./fetch_baserow_table.py --table_id 581 "${TMP_PATH}/Findings.xlsx"
+NO_PROXY="charite.de" python ./fetch_baserow_table.py --table_id 582 "${TMP_PATH}/Personnel.xlsx"
+NO_PROXY="charite.de" python ./fetch_baserow_table.py --table_id 660 "${TMP_PATH}/LB-Metadata.xlsx"
+
+PASSWORD="/opt/.backup_password"
+zip -r --password $(< /opt/.backup_password) $BACKUP_PATH/baserow_export_$(date +"%Y-%m-%d_%k-%M").zip $TMP_PATH
+
+rm -r "$TMP_PATH"
--- fetch_baserow_table.py
+++ fetch_baserow_table.py
@@ -3,6 +3,7 @@ from argparse import ArgumentParser
 import json
 
 from python_baserow_simple import BaserowApi
+import pandas as pd
 
 BR = BaserowApi(token_path=".baserow_token")
 
@@ -15,8 +16,20 @@ def main():
 
     data = BR.get_data(args.table_id)
 
-    with open(args.output_file, "w") as outfile:
-        json.dump(data, outfile)
+    if args.output_file.lower().endswith(".json"):
+        with open(args.output_file, "w") as outfile:
+            json.dump(data, outfile)
+    elif args.output_file.lower().endswith(".csv"):
+        records = [{"BaserowID": k, **v} for k, v in data.items()]
+        df = pd.DataFrame.from_records(records)
+        df.to_csv(args.output_file, index=False)
+    elif args.output_file.lower().endswith(".xlsx"):
+        records = [{"BaserowID": k, **v} for k, v in data.items()]
+        df = pd.DataFrame.from_records(records)
+        df.to_excel(args.output_file, index=False)
+    else:
+        raise RuntimeError(f"Unknown file format in output path: {args.output_file}")
+
 
 if __name__ == "__main__":
     main()
