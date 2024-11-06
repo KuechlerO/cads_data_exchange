071f889c3a83757b1f8954759a9a7b3be893602d
Oliver Küchler <oliver.kuechler@charite.de>
2024-11-06 13:32:03 +0100

    Make baserow backup script executable

--- run_baserow_backup.sh
+++ run_baserow_backup.sh
@@ -2,7 +2,6 @@
 BACKUP_DESTINATION=/data01/baserow_backups
 
 FILENAME="baserow_backup-$(date -I).tar.gz"
-cd "$(dirname "$0")"
-docker compose down
-docker compose run --rm -v $BACKUP_DESTINATION:/baserow/backups baserow backend-cmd-with-db backup  -f /baserow/backups/$FILENAME
-docker compose up -d
+cd "$1"
+docker compose run -v $BACKUP_DESTINATION:/baserow/backups baserow backend-cmd-with-db backup -f /baserow/backups/$FILENAME
+
